57900655245b900c25a2eb4fa4755475d4b92238
guenhyuck <guenhyuck94@gmail.com>
2023-04-07 15:54:48 +0900

    리팩토링,DBUtil 객체화 x.메인페이지 추가

--- src/main/java/com/KoreaIT/java/jam/servlet/ArticleListServlet.java
+++ src/main/java/com/KoreaIT/java/jam/servlet/ArticleListServlet.java
@@ -1,36 +1,29 @@
-package com.KoreaIT.java.jam;
+package com.KoreaIT.java.jam.servlet;
 
 import java.io.IOException;
 import java.sql.Connection;
+
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.KoreaIT.java.jam.util.DBUtil;
 
 @WebServlet("/article/list")
 public class ArticleListServlet extends HttpServlet {
-
-
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
 		response.setContentType("text/html; charset=UTF-8");
-
 		// DB 연결
 		String url = "jdbc:mysql://127.0.0.1:3306/JSPAM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
 		String user = "root";
 		String password = "";
 		Connection conn = null;
-
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
@@ -38,20 +31,19 @@ public class ArticleListServlet extends HttpServlet {
 			System.out.println("프로그램을 종료합니다");
 			return;
 		}
-
 		try {
 			conn = DriverManager.getConnection(url, user, password);
 
 			response.getWriter().append("Success!!!");
 
-			DBUtil dbUtil = new DBUtil(request, response);
+			String sql = "SELECT * FROM article ORDER BY id DESC;";
 
-			String sql = "SELECT * FROM article;";
-
-			List<Map<String, Object>> articleRows = dbUtil.selectRows(conn, sql);
+			List<Map<String, Object>> articleRows = DBUtil.selectRows(conn, sql);
 
 			response.getWriter().append(articleRows.toString());
 
+			request.setAttribute("articleRows", articleRows);
+			request.getRequestDispatcher("/jsp/article/list.jsp").forward(request, response);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
@@ -64,5 +56,4 @@ public class ArticleListServlet extends HttpServlet {
 			}
 		}
 	}
-
 }
