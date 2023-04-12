4226b814475f643e63dccf78975cbcaec38e334f
guenhyuck <guenhyuck94@gmail.com>
2023-04-12 17:58:17 +0900

    Articld Dto 도입

--- src/main/java/com/KoreaIT/java/jam/controller/ArticleController.java
+++ src/main/java/com/KoreaIT/java/jam/controller/ArticleController.java
@@ -3,15 +3,16 @@ package com.KoreaIT.java.jam.controller;
 import java.io.IOException;
 import java.sql.Connection;
 import java.util.List;
-import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.KoreaIT.java.jam.dto.Article;
 import com.KoreaIT.java.jam.service.ArticleService;
 
-
 public class ArticleController {
+
 	private HttpServletRequest request;
 	private HttpServletResponse response;
 	private Connection conn;
@@ -21,26 +22,24 @@ public class ArticleController {
 		this.conn = conn;
 		this.request = request;
 		this.response = response;
-
 		this.articleService = new ArticleService(conn);
 	}
 
 	public void showList() throws ServletException, IOException {
-
 		int page = 1;
 		if (request.getParameter("page") != null && request.getParameter("page").length() != 0) {
 			page = Integer.parseInt(request.getParameter("page"));
 		}
-
 		int itemsInAPage = articleService.getItemsInAPage();
 
 		int totalPage = articleService.getTotalPage();
 
-		List<Map<String, Object>> articleRows = articleService.getForPrintArticleRows(page);
+		List<Article> articles = articleService.getForPrintArticles(page);
 
 		request.setAttribute("page", page);
-
-		request.setAttribute("articleRows", articleRows);
+		request.setAttribute("totalPage", totalPage);
+		request.setAttribute("articles", articles);
 		request.getRequestDispatcher("/jsp/article/list.jsp").forward(request, response);
 	}
+
 }
--- src/main/java/com/KoreaIT/java/jam/service/ArticleService.java
+++ src/main/java/com/KoreaIT/java/jam/service/ArticleService.java
@@ -1,41 +1,39 @@
 package com.KoreaIT.java.jam.service;
-
 import java.sql.Connection;
 import java.util.List;
 import java.util.Map;
 
 import com.KoreaIT.java.jam.dao.ArticleDao;
+import com.KoreaIT.java.jam.dto.Article;
 
 public class ArticleService {
 	private Connection conn;
-	private ArticleDao articleDao;
 
+  
+  
+	private ArticleDao articleDao;
 	public ArticleService(Connection conn) {
 		this.conn = conn;
 		this.articleDao = new ArticleDao(conn);
 	}
-
 	public int getItemsInAPage() {
-
 		return 10;
 	}
-
 	public int getTotalPage() {
 		int itemsInAPage = getItemsInAPage();
-
 		int totalCnt = articleDao.getTotalCnt();
 		int totalPage = (int) Math.ceil((double) totalCnt / itemsInAPage);
 		return totalPage;
 	}
 
-	public List<Map<String, Object>> getForPrintArticleRows(int page) {
+	public List<Article> getForPrintArticles(int page) {
 		int itemsInAPage = getItemsInAPage();
 
 		int limitFrom = (page - 1) * itemsInAPage;
 
-		List<Map<String, Object>> articleRows = articleDao.getArticleRows(limitFrom, itemsInAPage);
+		List<Article> articles = articleDao.getArticles(limitFrom, itemsInAPage);
 
-		return articleRows;
+		return articles;
 	}
 
 }
